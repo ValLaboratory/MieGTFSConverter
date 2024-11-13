ba5cbbe93a0736b6f6d715ac11f2db3ab1f72afb
valkubo <kubo@val.co.jp>
2024-11-13 22:16:52 +0900

    デリバリの自動化

---
+++ MieGTFSConverter/delivery.ps1
@@ -0,0 +1,10 @@
+$target = "MieGTFSConverter.exe"
+$path = "\\contents-storage\horus\tools\路線バス_運賃取込\三重_GTFSコンバーター\検証版\" + $target
+$v1 = (Get-ItemProperty $path).VersionInfo.FileVersion
+$v2 = (Get-ItemProperty $target).VersionInfo.FileVersion
+if ( !( $v1 -eq $v2 ) ) {
+  $result = [System.Windows.Forms.MessageBox]::Show("検証版にリリースしますか？",$v2,"YesNo","Question","Button2")
+  if ($result -eq "Yes") {
+    Copy-Item -Path $target -Destination $path
+  }
+}
