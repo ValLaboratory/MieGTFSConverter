9f0901ba1daface9e96773b8767cbb0a6464ce2d
valkubo <kubo@val.co.jp>
2024-11-15 16:29:06 +0900

    デリバリの場所を変更

--- MieGTFSConverter/delivery.ps1
+++ MieGTFSConverter/delivery.ps1
@@ -1,5 +1,5 @@
 $target = "MieGTFSConverter.exe"
-$path = "\\contents-storage\horus\tools\路線バス_運賃取込\三重_GTFSコンバーター\検証版\" + $target
+$path = "\\contents-storage\horus\tools\路線バス_ダイヤ取込\三重_GTFSコンバーター\検証版\" + $target
 $v1 = (Get-ItemProperty $path).VersionInfo.FileVersion
 $v2 = (Get-ItemProperty $target).VersionInfo.FileVersion
 if ( !( $v1 -eq $v2 ) ) {
