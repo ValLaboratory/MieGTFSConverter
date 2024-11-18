57d095dd889cfe1f5f4b9040aea2b453cb97625e
valkubo <kubo@val.co.jp>
2024-11-18 15:03:54 +0900

    デリバリ自動化シェル  サーバーにファイルがないときに対応

--- MieGTFSConverter/delivery.ps1
+++ MieGTFSConverter/delivery.ps1
@@ -1,9 +1,16 @@
 $target = "MieGTFSConverter.exe"
 $path = "\\contents-storage\horus\tools\路線バス_ダイヤ取込\三重_GTFSコンバーター\検証版\" + $target
-$v1 = (Get-ItemProperty $path).VersionInfo.FileVersion
-$v2 = (Get-ItemProperty $target).VersionInfo.FileVersion
-if ( !( $v1 -eq $v2 ) ) {
-  $result = [System.Windows.Forms.MessageBox]::Show("検証版にリリースしますか？",$v2,"YesNo","Question","Button2")
+$v1 = (Get-ItemProperty $target).VersionInfo.FileVersion
+if( Test-Path $path ){
+  $v2 = (Get-ItemProperty $path).VersionInfo.FileVersion
+  if ( !( $v1 -eq $v2 ) ) {
+    $result = [System.Windows.Forms.MessageBox]::Show("検証版にリリースしますか？",$v1,"YesNo","Question","Button2")
+    if ($result -eq "Yes") {
+      Copy-Item -Path $target -Destination $path
+    }
+  }
+} else {
+  $result = [System.Windows.Forms.MessageBox]::Show("検証版にリリースしますか？",$v1,"YesNo","Question","Button2")
   if ($result -eq "Yes") {
     Copy-Item -Path $target -Destination $path
   }
